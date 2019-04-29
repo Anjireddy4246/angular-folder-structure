718e8a3dbd48cc3dcf688fa7c8278a9a279bc17a
akata <next@969507>
2019-04-29 23:09:59 +0530

    initial version

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CoreRoutingModule } from './core-routing.module';
+
+@NgModule({
+    imports: [CoreRoutingModule],
+    declarations: [],
+    bootstrap: []
+})
+export class CoreModule { }
